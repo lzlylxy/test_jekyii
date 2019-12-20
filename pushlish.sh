efb0d022d2eb4cd1988b1a3c32134de10da9b204
Zale Lee <lzlylxy@gmail.com>
2019-12-20 18:43:56 +0800

    update

---
+++ pushlish.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "start jeky build"
+jekyll build
+
+
+echo "start update git"
+git add .
+git commit "update"
+git push
+
+
+echo "login server and update code"
+#ssh -i /path/to/key username@ip > /dev/null 2>&1 << eeooff
+#
+#cd "/path/to/code_path"
+#git pull
+#
+#eeooff
+
+echo "done"
